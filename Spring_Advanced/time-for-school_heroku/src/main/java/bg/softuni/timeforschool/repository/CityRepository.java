9ace2c417877e904f68ff6cb171bf8b3586ac57e
ValeriDimov <89461040+ValeriDimov@users.noreply.github.com>
2022-08-21 15:53:59 +0300

    Delete Spring_Advanced/time-for-school_heroku directory

--- Spring_Advanced/time-for-school_heroku/src/main/java/bg/softuni/timeforschool/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.timeforschool.repository;
-
-import bg.softuni.timeforschool.model.entity.CityEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends JpaRepository<CityEntity, Long> {
-}
--- Spring_Advanced/time-for-school_heroku/src/main/java/bg/softuni/timeforschool/web/RestOffersController.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.timeforschool.web;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RestOffersController {
-    //TODO: implement
-
-}
