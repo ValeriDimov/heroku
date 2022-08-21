43eecedaa42a4435464f8635f8bc772e253cb98b
ValeriDimov <89461040+ValeriDimov@users.noreply.github.com>
2022-08-21 15:44:34 +0300

    Delete Spring_Advanced/time-for-school directory

--- Spring_Advanced/time-for-school/src/main/java/bg/softuni/timeforschool/repository/DistrictRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.timeforschool.repository;
-
-import bg.softuni.timeforschool.model.entity.DistrictEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistrictRepository extends JpaRepository<DistrictEntity, Long> {
-}
--- Spring_Advanced/time-for-school/src/main/java/bg/softuni/timeforschool/repository/SchoolProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.timeforschool.repository;
-
-import bg.softuni.timeforschool.model.entity.SchoolProfileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchoolProfileRepository extends JpaRepository<SchoolProfileEntity, Long> {
-}
